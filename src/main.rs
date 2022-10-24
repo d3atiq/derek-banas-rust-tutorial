7ca38a2002b50a3b02962d20882c4f9112e4cb72
Douglas Atique <douglas.atique@d-3.dev>
2022-10-24 09:30:37 -0300

    Match with Ordering

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let age2 = 8;
-    match age2 {
-        1..=18 => println!("Important Birthday"),
-        21 | 50 => println!("Important Birthday"),
-        65..=i32::MAX => println!("Important Birthday"),
-        _ => println!("Not an Important Birthday"),
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
     };
 }
