b1c68f8324e3d3733494171aa2327f315502535d
Douglas Atique <douglas.atique@d-3.dev>
2022-10-24 09:42:41 -0300

    Basic tuples

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,9 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let arr_1 = [1,2,3,4];
-    println!("1st: {}", arr_1[0]);
-    println!("Length: {}", arr_1.len());
-    let arr_2 = [1,2,3,4,5,6,7,8,9];
-    let mut loop_idx = 0;
-    loop {
-        if arr_2[loop_idx] % 2 == 0 {
-            loop_idx += 1;
-            continue;
-        }
-        if arr_2[loop_idx] == 9 {
-            break;
-        }
-        println!("Val : {}", arr_2[loop_idx]);
-        loop_idx += 1;
-    }
+    let my_tuple: (u8, String, f64) = (47, "Derek".to_string(), 50_000.00);
 
-    loop_idx = 0;
-    while loop_idx < arr_2.len() {
-        println!("Arr : {}", arr_2[loop_idx]);
-        loop_idx += 1;
-    }
-
-    for val in arr_2.iter() {
-        println!("Val : {}", val);
-    };
-
+    println!("Name : {}", my_tuple.1);
+    let (v1, v2, v3) = my_tuple;
+    println!("Age : {}", v1);
 }
