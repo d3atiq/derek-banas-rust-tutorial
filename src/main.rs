5ddafb30adb036c8ec302c677ed2cae060b7fe86
Douglas Atique <douglas.atique@d-3.dev>
2022-10-25 15:33:00 -0300

    Basics of enums

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,36 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let int_u8: u8 = 5;
-    let int2_u8: u8 = 4;
-    let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
-    println!("{}", int3_u32);
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+
+    impl Day {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true,
+                _ => false,
+            }
+        }
+    }
+
+    let today: Day = Day::Monday;
+    match today 
+    {
+        Day::Monday => println!("Everyone hates Monday"),
+        Day::Tuesday => println!("Donut day"),
+        Day::Wednesday => println!("Hump day"),
+        Day::Thursday => println!("Pay day"),
+        Day::Friday => println!("Almost Weekend"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    }
+
+    println!("Is today the weekend {}", today.is_weekend());
 }
