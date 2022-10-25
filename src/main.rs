aeb968e5714cd1dc9296e2d7bee5715dffebc39b
Douglas Atique <douglas.atique@d-3.dev>
2022-10-25 15:42:25 -0300

    Vector basics

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,22 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    enum Day {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday,
+    let vec1: Vec< i32 > = Vec::new();
+    let mut vec2 = vec![1,2,3,4];
+    vec2.push(5);
+    println!("1st : {}", vec2[0]);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd : {}", second),
+        None => println!("No 2nd value"),
     }
-
-    impl Day {
-        fn is_weekend(&self) -> bool {
-            match self {
-                Day::Saturday | Day::Sunday => true,
-                _ => false,
-            }
-        }
+    for i in &mut vec2 {
+        *i *= 2;
     }
-
-    let today: Day = Day::Monday;
-    match today 
-    {
-        Day::Monday => println!("Everyone hates Monday"),
-        Day::Tuesday => println!("Donut day"),
-        Day::Wednesday => println!("Hump day"),
-        Day::Thursday => println!("Pay day"),
-        Day::Friday => println!("Almost Weekend"),
-        Day::Saturday => println!("Weekend"),
-        Day::Sunday => println!("Weekend"),
+    for i in &vec2 {
+        println!("{}", i);
     }
+    println!("Vec length {}", vec2.len());
+    println!("Pop: {:?}", vec2.pop());
 
-    println!("Is today the weekend {}", today.is_weekend());
 }
