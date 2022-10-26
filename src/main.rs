005429fc095e017902dc71d1521c13b433775340
Douglas Atique <douglas.atique@d-3.dev>
2022-10-26 09:39:26 -0300

    Function returning expression

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn get_sum(x: i32, y: i32) {
-    println!("{} + {} = {}", x, y, x+y);
+fn get_sum_2(x: i32, y: i32) -> i32 {
+    x + y
 }
 
 fn main() {
-    get_sum(5, 4);
+    println!("{}", get_sum_2(5, 4));
 }
